238dcd007864e98fab774e04160ba4bb11857619
KelyKley <kelyanamurohuamani@gmail.com>
2017-10-10 12:35:49 -0500

    Subiendo primera parte Home

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import logo from "./logo-white.png";
+import "./Home.css";
 import {
 	BrowserRouter,
 	Route,
@@ -10,13 +12,26 @@ import {
 const Home = ( {model} ) => {
 
 	return (
-		<div>
-			<NavLink to={"/signup"}
-			         className="btn btn-lyft btn-lg btn-block"
-			>
-				Sign Up
-			</NavLink>
-		</div>
+<section id="portada">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12 col-xs-12">
+            <h1>
+              <img src={logo} width="150px;" alt="logo-white" />
+            </h1>
+          </div>
+        </div>
+        <div className="row botones">
+          <div className="col-md-6 col-xs-6">
+		  <NavLink to={"/signup"}className="btn btn-lyft btn-lg btn-block">Login</NavLink>
+		  
+          </div>
+          <div className="col-md-6 col-xs-6 btnn">
+		  <NavLink to={"/signup"}className="btn btn-lyft btn-lg btn-block">Sign Up</NavLink>
+          </div>
+        </div>
+      </div>
+    </section>
 	);
 }
 
