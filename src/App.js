8c52f33eb1d7d38ebccfe00273186ac4126a2bb6
gabiprds93 <gabiprds.93@gmail.com>
2017-10-10 11:25:05 -0500

    Redireccion al Home

--- src/App.js
+++ src/App.js
@@ -26,13 +26,13 @@ const App = (props) => {
 		<div>
 			<Switch>
 				<Route exact path="/"
-				       render={() => <Redirect to= {'/home'}/>}/>
+				    render={() => <Redirect to= {'/home'}/>}/>
+				<Route exact path="/Proyecto-Lyft"
+					render={() => <Redirect to= {'/home'}/>}/>
 				<Route  path="/home" render={() => <Home model={model} />}/>
 				<Route  path="/signup" render={() => <SignUp model={model} />}/>
 				<Route  path="/signup-form" render={() => <SignUpForm model={model} />}/>
-
 				<Route  path="/lyftmap" render={() => <LyftMap model={model} />}/>
-
 				<Route component={NotFound}/>
 			</Switch>
 		</div>
