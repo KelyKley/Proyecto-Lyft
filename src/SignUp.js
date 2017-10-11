4c3007d1d5f7ecf2a2a70d069bbe567ec5a53db7
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-11 13:55:13 -0500

    Separando en componentes

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import {
-	BrowserRouter,
-	Route,
-	Switch,
 	NavLink,
-	Redirect
 } from 'react-router-dom'
 import ReactBootstrap from 'react-bootstrap';
-import { Button, Grid, Col, Row, InputGroup, FormControl ,Dropdown, MenuItem} from 'react-bootstrap';
-
+import { Grid, Col, Row, InputGroup, FormControl ,Dropdown, MenuItem} from 'react-bootstrap';
 import './SignUp.css';
-import model from './index.js';
 
-class SignUp extends Component {
+class SignUp extends Component{
+	render () {
+		const {model} = this.props;
+		return(
+			<div className="container-fluid">
+				<Header/>
+				<hr/>
+				<SignUpPhone model={model}/>
+			</div>
+		);
+	};
+}
+
+class Header extends Component{
+	render() {
+		return (
+			<div>
+				<Grid>
+				<Row className="text-center head">
+					<Col xs={12} md={12}>
+					<Row>
+						<Col xs={2} md={2} className='back'>
+							<NavLink to={"/home"}>
+								<i className="fa fa-angle-left fa-3x" aria-hidden="true"/>
+							</NavLink>
+						</Col>
+						<Col xs={8} md={8}>
+							<h1>Sign up</h1>
+							<h5 className="text-center"> Join now for free ride credit</h5>
+						</Col>
+					</Row>
+					</Col>
+				</Row>
+				</Grid>
+			</div>
+		);
+	};
+}
+
+class SignUpPhone extends Component {
 	constructor(props) {
 		super(props);
 		this.flag = ["https://alianzapacifico.net/wp-content/uploads/2015/03/2000px-Flag_of_Peru_1825_-_1950.svg_.png", "http://flagpedia.net/data/flags/normal/mx.png", "https://as01.epimg.net/img/comunes/fotos/fichas/paises/svg/chi.svg"];
@@ -20,15 +53,13 @@ class SignUp extends Component {
 		this.code = ["+51", "+56", "+52"];
 		this.state = {
 			selectValue: 'Perú',
-			flag: this.flag[0],
 			flagDrop: this.flag[0],
-			code: this.code[0],
 			codeDrop: this.code[0],
 			phone: "",
 			activeNext: false,
 		}
 	}
-
+	
 	render() {
 		const {model} = this.props;
 
@@ -63,30 +94,12 @@ class SignUp extends Component {
 				);
 		});
 		return (
-			<div className="container-fluid">
-				<Grid>
-				<Row className="text-center head">
-					<Col xs={12} md={12}>
-					<Row>
-						<Col xs={2} md={2} className='back'>
-							<NavLink to={"/home"}>
-								<i className="fa fa-angle-left fa-3x" aria-hidden="true"/>
-							</NavLink>
-						</Col>
-						<Col xs={8} md={8}>
-							<h1>Sign up</h1>
-							<h5 className="text-center"> Join now for free ride credit</h5>
-						</Col>
-					</Row>
-					</Col>
-				</Row>
-				</Grid>
-				<hr/>
+			<div>
 				<section className="container signUp-phone text-center">
 					<p><em>Get riding within minutes</em></p>
 					<InputGroup>
 						<FormControl
-							componentClass="dropdownselect"
+							componentClass="dropdown"
 							className="dropdown"
 						>
 							<Dropdown id="dropdown-custom-1">
@@ -108,18 +121,29 @@ class SignUp extends Component {
 					
 					</div>
 				</section>
-				{
-				this.state.activeNext ?
-					<NavLink
-						to={"/send-code"}
-						className="btn btn-lg btn-block btn-lyft">Next</NavLink>
-					:
-					<button
-						className="btn btn-lg btn-block btn-lyft disabled">Next</button>
-				}
+				<BtnNext flag={this.state.activeNext}/>
 			</div>
 		);
 	}
+}
+
+class BtnNext extends Component {
+	render () {
+		const {flag} = this.props;
+		return (
+			<div>
+				{
+					flag ?
+						<NavLink
+							to={"/send-code"}
+							className="btn btn-lg btn-block btn-lyft">Next</NavLink>
+						:
+						<button
+							className="btn btn-lg btn-block btn-lyft disabled">Next</button>
+				}
+			</div>
+		);
+	};
 }
 
 export default SignUp;
